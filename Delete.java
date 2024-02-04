4d06bae50f1a425b6c50ca4202d30e656f8c5c9f
BidayeYash <144249501+BidayeYash@users.noreply.github.com>
2024-02-04 22:06:41 +0530

    Add files via upload

---
+++ Delete.java
@@ -0,0 +1,42 @@
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class Delete extends JFrame {
+JLabel label1;
+JTextField field1;
+JButton button;
+
+public Delete() {
+setLayout(new FlowLayout());
+label1 = new JLabel("Employee ID");
+field1 = new JTextField(10);
+button = new JButton("Delete Employee");
+button.addActionListener(new ActionListener() {
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		dispose();
+		System.out.println("Deleted");
+		
+	}
+});
+add(label1);
+add(field1);
+add(button);
+setSize(400,400);
+setVisible(true);
+
+
+
+
+}
+public static void main(String[] args) {
+	new Delete();
+}
+}
